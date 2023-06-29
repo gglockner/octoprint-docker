c0eb66811a22c66e523b41f1d2de551e0099188c
Greg Glockner <greg@glockners.net>
2023-06-28 22:36:21 -0700

    Fix permissions on printer device

--- setup.sh
+++ setup.sh
@@ -7,6 +7,3 @@ then
   cp /etc/apt/trusted.gpg.d/raspberrypi-archive-stable.gpg .
   cp /etc/apt/sources.list.d/raspi.list .
 fi
-
-# Make the printer device readable/writeable for the Docker container
-sudo chmod a+rw /dev/ttyACM0
